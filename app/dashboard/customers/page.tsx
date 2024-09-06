55b90ad2ec53deac86cbd0cb563552a4225f94aa
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-09-06 22:31:08 +0200

    all-set

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { Suspense } from "react";
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
+import { Query } from "@vercel/postgres";
 const page = () => {
   return (
-    <p>
-      customers
-    </p>
-  )
-}
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+        <CreateInvoice />
+      </div>
+      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div> */}
+    </div>
+  );
+};
 
-export default page
+export default page;
